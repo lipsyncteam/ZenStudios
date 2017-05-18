7fb950a29f52809400d25f4088fe6c48128f411c
neeraj-bedi <neerajbedico@gmail.com>
2017-05-18 20:54:56 +0530

    Testing Controller
    #Added Basic Control Script
    #Added Character Image for test

---
+++ Assets/OVR_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVR_Controller : MonoBehaviour {
+
+
+
+    private bool trigger_is;
+    private RaycastHit HitInfo;
+    public GameObject Cube;
+	void Start ()
+    {
+        
+
+    }
+	
+	
+	void Update ()
+    {
+        Vector3 forwardSight = transform.TransformDirection(transform.forward);
+        trigger_is = OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger);
+        if(Physics.SphereCast(OVRInput.GetLocalControllerPosition(OVRInput.Controller.Remote),1f,forwardSight, out HitInfo, 10f))
+        {
+            print(HitInfo.collider.gameObject);
+            if(HitInfo.collider.tag == "Scroll")
+            {
+                Cube.GetComponent<Renderer>().material.color = Color.red;
+            }else
+            {
+                Cube.GetComponent<Renderer>().material.color = Color.white;
+
+            }
+        }
+        print(trigger_is);
+    }
+}
